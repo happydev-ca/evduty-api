0797e9e7c8f8ead7b05874afb5aabfbbb4bd8a95
Francois Perron <fperron@gmail.com>
2024-02-04 23:05:59 -0500

    Document public methods return type

--- evdutyapi/evduty_api.py
+++ evdutyapi/evduty_api.py
@@ -1,7 +1,9 @@
 from datetime import timedelta, datetime
+from typing import List
 
 import aiohttp
 
+from evdutyapi import Station
 from evdutyapi.api_response.charging_session_response import ChargingSessionResponse
 from evdutyapi.api_response.station_response import StationResponse
 
@@ -16,7 +18,7 @@ class EVDutyApi:
         self.headers = {'Content-Type': 'application/json'}
         self.expires_at = datetime.now() - timedelta(seconds=1)
 
-    async def async_authenticate(self):
+    async def async_authenticate(self) -> None:
         if datetime.now() < self.expires_at:
             return
 
@@ -27,7 +29,7 @@ class EVDutyApi:
             self.headers['Authorization'] = 'Bearer ' + body['accessToken']
             self.expires_at = datetime.now() + timedelta(seconds=body['expiresIn'])
 
-    async def async_get_stations(self):
+    async def async_get_stations(self) -> List[Station]:
         await self.async_authenticate()
         async with self.session.get(f'{self.base_url}/v1/account/stations', headers=self.headers) as response:
             response.raise_for_status()
@@ -36,7 +38,7 @@ class EVDutyApi:
             await self.__async_get_sessions(stations)
             return stations
 
-    async def __async_get_sessions(self, stations):
+    async def __async_get_sessions(self, stations) -> None:
         for station in stations:
             for terminal in station.terminals:
                 async with self.session.get(f'{self.base_url}/v1/account/stations/{station.id}/terminals/{terminal.id}/session', headers=self.headers) as response:
