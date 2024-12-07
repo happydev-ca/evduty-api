74fb34efbe9f0e3f81088a364bd361777f779bfd
Francois Perron <fperron@gmail.com>
2024-12-07 11:04:30 -0500

    Remove station status field
    Since it cant be computed from terminals statuses and is
    not provided when there's no terminals.

--- evdutyapi/station.py
+++ evdutyapi/station.py
@@ -1,18 +1,16 @@
-from evdutyapi import Terminal, ChargingStatus
+from evdutyapi import Terminal
 
 
 class Station:
-    def __init__(self, id: str, name: str, status: ChargingStatus, terminals: list[Terminal]):
+    def __init__(self, id: str, name: str, terminals: list[Terminal]):
         self.id = id
         self.name = name
-        self.status = status
         self.terminals = terminals
 
     def __repr__(self) -> str:
-        return f"<Station id:{self.id} name:{self.name} status:{self.status} terminals:{len(self.terminals)}>"
+        return f"<Station id:{self.id} name:{self.name} terminals:{len(self.terminals)}>"
 
     def __eq__(self, __value):
         return (self.id == __value.id and
                 self.name == __value.name and
-                self.status == __value.status and
                 self.terminals == __value.terminals)
--- test/api_response/test_station_response.py
+++ test/api_response/test_station_response.py
@@ -1,22 +1,20 @@
 import unittest
 
-from evdutyapi import ChargingStatus
 from evdutyapi.api_response.station_response import StationResponse
 
 
 class StationResponseTest(unittest.TestCase):
     def test_parses_json(self):
-        json = StationResponse(id="1", name="A", status="available", terminals=[]).to_json()
+        json = StationResponse(id="1", name="A", terminals=[]).to_json()
 
         station = StationResponse.from_json(json)
 
         self.assertEqual(station.id, "1")
         self.assertEqual(station.name, "A")
-        self.assertEqual(station.status, ChargingStatus.available)
         self.assertEqual(station.terminals, [])
 
     def test_set_empty_terminals_on_default(self):
-        json = StationResponse(id="1", name="A", status="available", terminals=None).to_json()
+        json = StationResponse(id="1", name="A", terminals=None).to_json()
 
         station = StationResponse.from_json(json)
 
