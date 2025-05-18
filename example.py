6d686e1c49824b33c9b55744035636da9d4286a5
Francois Perron <fperron@gmail.com>
2025-05-18 09:03:51 -0400

    Simplify api usage examples

--- example.py
+++ example.py
@@ -11,11 +11,5 @@ async def run():
         stations = await api.async_get_stations()
         for station in stations:
             print(station)
-            for terminal in station.terminals:
-                print(terminal)
-                print(terminal.session)
-                print(terminal.network_info)
-                print(terminal.charging_profile)
-
 
 asyncio.run(run())
--- example_set_max_charging_current.py
+++ example_set_max_charging_current.py
@@ -9,18 +9,25 @@ password = os.environ['PASSWORD']
 current = int(os.environ['CURRENT'])
 print('will set max current to', current)
 
+
 async def run():
     async with aiohttp.ClientSession() as session:
         api = EVDutyApi(email, password, session)
-        stations = await api.async_get_stations()
 
-        station = stations[0]
-        print(station)
-
-        terminal = station.terminals[0]
-        print(terminal)
-
+        terminal = await get_first_terminal(api)
         print(terminal.charging_profile)
+
         await api.async_set_terminal_max_charging_current(terminal, current=current)
+
+        terminal = await get_first_terminal(api)
+        print(terminal.charging_profile)
+
+
+async def get_first_terminal(api):
+    stations = await api.async_get_stations()
+    station = stations[0]
+    terminal = station.terminals[0]
+    return terminal
+
 
 asyncio.run(run())
